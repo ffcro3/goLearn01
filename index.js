54c795dcfcdd843947b52f31b1f990a7448d9a67
Fabricio Rocha <fabriciofrocha87@gmail.com>
2019-08-19 22:41:30 -0300

    Routes Test

--- index.js
+++ index.js
@@ -2,9 +2,33 @@ const express = require("express");
 
 const server = express();
 
+//ROUTE
 server.get("/home", (req, res) => {
   return res.json({
     message: "Hello World"
+  });
+});
+
+//ROUTE WITH QUERY PARAMS
+server.get("/welcome", (req, res) => {
+  const nome = req.query.nome;
+
+  if (!nome) {
+    return res.json({
+      message: "Usuário não definido. Por favor digite o seu nome"
+    });
+  }
+  return res.json({
+    message: `Hello ${nome}`
+  });
+});
+
+//ROUTE WITH ROUTE PARAMS
+server.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  return res.json({
+    message: `Dados do usuário ${id}`
   });
 });
 
